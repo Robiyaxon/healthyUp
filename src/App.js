e986c2bfbfc4322546d06a7a53ec5a4faf13fe6f
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-05-13 12:21:57 +0500

    create file and folders

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import { DatePicker } from "antd";
+import { Home } from "./components/home/Home";
+
 function App() {
   return (
     <div>
       <DatePicker />
       hello world !
+      <Home />
     </div>
   );
 }
