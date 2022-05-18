0c07e1e7dd553ae0274f55fd1a50a03cfe4f9376
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-05-18 11:12:43 +0500

    top header is complete

--- src/components/top_header/TopHeader.jsx
+++ src/components/top_header/TopHeader.jsx
@@ -1,28 +1,53 @@
 import React, { useState } from "react";
 import style from "./TopHeader.module.css";
 import logo from "../../assets/TopHeader/logo.svg";
+import navbarOpen from "../../assets/TopHeader/navbaricon.svg";
 import i18n from "./../../i18n";
+import { Drawer } from "antd";
 export const TopHeader = () => {
   const defaultLang = localStorage.getItem("lang") || "uz";
+  const [lang, setLang] = useState(defaultLang);
   function handleChange(value) {
     console.log(value); // { value: "lucy", key: "lucy", label: "Lucy (101)" }
     setLang(value.target.value);
     localStorage.setItem("lang", value.target.value);
     i18n.changeLanguage(value.target.value);
   }
-  const [lang, setLang] = useState(defaultLang);
+  const [visible, setVisible] = useState(false);
 
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
   return (
     <div className={style.Wrapper}>
+      <Drawer
+        placement="right"
+        onClose={onClose}
+        visible={visible}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
       <img src={logo} alt="" />
-      <div>
+      <div className={style.Drawer}>
         <div className={style.select}>
           <select name="lang" value={lang} onChange={handleChange}>
             <option value="uz">UZ</option>
             <option value="ru">RU</option>
-            <option value="eng">EN</option>
+            <option value="eng">ENG</option>
           </select>
         </div>
+        <img
+          src={navbarOpen}
+          className={style.NavbarIcon}
+          alt=""
+          onClick={showDrawer}
+        />
       </div>
     </div>
   );
