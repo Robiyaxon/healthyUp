7be5fff849bf9e812f88bb52128832610b0f414e
SulaymonAbdukarimov <sulaymonabdukarimov2@gmail.com>
2022-05-19 10:08:40 +0500

    -

--- src/components/home/header/Header.jsx
+++ src/components/home/header/Header.jsx
@@ -3,31 +3,11 @@ import React from "react";
 import main from "../../../assets/home/header/main.png";
 
 import styles from "./Header.module.css";
-<<<<<<< HEAD
-=======
-import { useTranslation } from 'react-i18next';
->>>>>>> 04fbb36230fae9a867d8d18879c990940aa96230
+import { useTranslation } from "react-i18next";
 
 export const Header = () => {
   const { t } = useTranslation();
   return (
-<<<<<<< HEAD
-    <div className={styles.header}>
-      <div className={styles.header__img}>
-        <img src={main} alt="header img" />
-      </div>
-      <div className={styles.header__text_block}>
-        <h1 className={styles.header__text_block__title}>
-          Vazin <strong> yo‘qoyishni </strong> istaysizmi
-        </h1>
-        <p className={styles.header__text_block__text}>
-          Parhezshunoslar va murabbiylar ham ro'yxatdan o'tishlari mumkin
-          saytimiz orqali o'z maslahatlarini berishadi.
-        </p>
-        <div className={styles.header__text_block__btn}>
-          <button>Kirish</button>
-          <button>Login</button>
-=======
     <>
       <div className={styles.header}>
         <div className={styles.header__img}>
@@ -63,7 +43,6 @@ export const Header = () => {
               </a>
             </div>
           </div>
->>>>>>> 04fbb36230fae9a867d8d18879c990940aa96230
         </div>
         <p className={styles.endText}>
           Har qanday vazn yo'qotish dasturiga amal qilishni boshlashdan oldin,
