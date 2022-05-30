0f83cd0e3934b9c236d280630248e415191088ec
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-05-30 11:39:34 +0500

    5 sabab

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { HomeHeader } from "./home_header/HomeHeader";
 import { MyCarousel } from "./carousel/Carousel";
+import { Cause } from "./Cause/Cause";
 
 export const Home = () => {
   return (
     <div>
       <HomeHeader />
+      <Cause/>
       <MyCarousel />
     </div>
   );
