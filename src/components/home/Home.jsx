dcc91ac98c0a139688ca71c2a70776b61bb1115d
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-05-31 12:31:49 +0500

    pull

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { HomeHeader } from "./home_header/HomeHeader";
 import { MyCarousel } from "./carousel/Carousel";
+import { Specialists } from "./specialists/Specialists";
 
 export const Home = () => {
   return (
     <div>
       <HomeHeader />
       <MyCarousel />
+      <Specialists /> 
     </div>
   );
 };
--- src/components/home/specialists/Specialists.jsx
+++ src/components/home/specialists/Specialists.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
+import p1 from '../../../assets/home/specialists/p1.png'
+
+import styles from './Specialists.module.css'
+
 export const Specialists = () => {
   return (
-    <div>
-      <h1>Mutaxassislar</h1>
-      <p>Saytimiz orqali eng yaxshi mutaxassislar orasiga qo'shiling</p>
+    <div className={styles.specialists}>
+      <h1 className={styles.title}>Mutaxassislar</h1>
+      <p className={styles.desc}>Saytimiz orqali eng yaxshi mutaxassislar orasiga qo'shiling</p>
+      <div className={styles.block}>
+        <div className={styles.box}>
+           <div className={styles.img}></div>
+        </div>
+        <div className={styles.box}>
+            <img src={p1} alt="" />
+        </div>
+        <div className={styles.box}>
+            <img src={p1} alt="" />
+        </div>
+      </div>
     </div>
   );
 };
