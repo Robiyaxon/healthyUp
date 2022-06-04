baac911f55fdcbb10d9d5afe179a8018a20c1efa
SulaymonAbdukarimov <sulaymonabdukarimov2@gmail.com>
2022-06-04 13:22:32 +0500

    about

--- src/components/about_us/AboutUs.jsx
+++ src/components/about_us/AboutUs.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import style from "./aboutUz.module.css";
 import man from "./../../assets/about_us/man.png";
 import girl from "./../../assets/about_us/girl.png";
-
+import Aos from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 export const AboutUs = () => {
+  useEffect(() => {
+    Aos.init({ once: true });
+  }, []);
   return (
     <div className={style.header_wrapper}>
       <div className={style.header}>
         {/* first */}
-        <div className={style.first}>
-          <div className={style.manWrapper}>
+        <div data-aos="zoom-in-up" className={style.first}>
+          <div data-aos="fade-up-right" className={style.manWrapper}>
             <img src={man} alt="man img" />
           </div>
-          <div className={style.first_content}>
+          <div
+            data-aos="fade-down"
+            data-aos-easing="linear"
+            data-aos-duration="1500"
+            className={style.first_content}
+          >
             <h2>Biz haqimizda</h2>
             <p>
               Maqsad bilan ozishga intilish, xatarli intilishdir. Chunki bunday
@@ -21,14 +31,18 @@ export const AboutUs = () => {
               bunday ozishni uddalay olmay harakatni to‘xtatsangiz, yaxshi
               holatda oldingi vazningizni ustiga yana 3 – 4 kg yuk olasiz. Yomon
               holatda bunday ochlik va stress tufayli gastrit yoki oshqozon
-              yarasi va depressiya kasalligi paydo bo‘lishi mumkin.{" "}
+              yarasi va depressiya kasalligi paydo bo‘lishi mumkin.
             </p>
           </div>
         </div>
 
         {/* second  */}
-        <div className={style.second}>
-          <div className={style.second_content}>
+        <div data-aos="zoom-in-down" className={style.second}>
+          <div
+            data-aos="fade-up"
+            data-aos-duration="3000"
+            className={style.second_content}
+          >
             <p>
               Shu sababli biz tog`ri ozishga yordam beruvchi bepul sayt
               yaratishga qaror qildik. bundan tashqari satimiz dietolog va
@@ -36,7 +50,7 @@ export const AboutUs = () => {
               beradi.
             </p>
           </div>
-          <div className={style.girlWrapper}>
+          <div data-aos="fade-up-left" className={style.girlWrapper}>
             <img src={girl} alt="" />
           </div>
         </div>
