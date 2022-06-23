60b64c6c5efc7a1fb8a15e36325de9d5812d1e22
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-06-23 20:59:38 +0500

    fix foods

--- src/components/login/foods/Foods.jsx
+++ src/components/login/foods/Foods.jsx
@@ -13,36 +13,57 @@ var config = {
     "Content-Type": "application/json",
   },
 };
-const Foods = React.memo((props) => {
+const Foods = (props) => {
 
   const { t } = useTranslation();
-  const [data, setData] = useState([]);
+  // const [data, setData] = useState([
+    
+  // ]);
   const navigate = useNavigate();
   const [texnikaModal, setTexnikaModal] = useState("");
-  
-  const app = []
+  const [app, setApp] = useState([
+    {id:1}
 
-  // app.push(texnikaModal)
+  ]);
+
+  const data = [
+    {id:1, name:"sdfgsdg"},
+    {id:2, name:"sdfgsdg"},
+    {id:3, name:"sdfgsdg"}
+  ]
+
+  // console.log(data);
+  
+  // const app = []
+
+
+  const appPush  = {id: texnikaModal}
+
   useEffect(() => {
-    axios(config)
-      .then(function (response) {
-        setData(response.data);
-      })
-      .catch(function (error) { });
+    let temp = app;
+    temp.push(texnikaModal)
+    setApp(temp);
+    // axios(config)
+    //   .then(function (response) {
+    //     // setData(response.data);
+    //   })
+    //   .catch(function (error) { });
   }, []);
   const clikc = () => {
     navigate("/femaleOrMale")
     props.setcan_not_dieta(texnikaModal)
   }
 
-  console.log(texnikaModal);
-  console.log(app);
+  console.log("ID", texnikaModal);
+
+  console.log("here", app);
   return (
     <div className={style.Foods}>
       <h1>{t("foods")}</h1>
       <div className={style.Select_block}>
         <div className={style.card}>
-          {data.map(a => <div className={style.Block_Card} onClick={() => app.push(a.id)
+          Hello
+          {data.map(a => <div className={style.Block_Card} onClick={() => setTexnikaModal(a.id)
           }>{a.name}</div>)}
         </div>
       </div>
@@ -56,5 +77,5 @@ const Foods = React.memo((props) => {
       </Button>
     </div>
   )
-})
+}
 export default Foods
