efae94167c8af91412928a738bd24dba039d3d6b
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-06-27 21:26:26 +0500

    push

--- src/components/login/foods/Foods.jsx
+++ src/components/login/foods/Foods.jsx
@@ -6,7 +6,8 @@ import { instance } from '../../../api/api';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-const Foods =  React.memo((props) => {
+import FoodItem from './FoodItem';
+const Foods = React.memo((props) => {
   const { t } = useTranslation();
   const [data, setData] = useState([]);
   const navigate = useNavigate();
@@ -16,40 +17,30 @@ const Foods =  React.memo((props) => {
   useEffect(() => {
     instance.get("product/").then((response) => setData(response.data));
   }, [setData]);
-  console.log(selectedList);
   const click = async (id) => {
+
     let temp = app;
     temp.push(id)
     setApp(temp);
     setSelectedList(id)
+    return data.filter(item => item.id !== id)
   }
   const click2 = () => {
     navigate("/femaleOrMale")
     props.setcan_not_dieta(app)
+    // return data.filter(item=>item.id!==id)
   };
+
+  const deleteCard =(id)=>setData(data => data.filter(item=>item.id!==id))
   
-    // const deleteCard =(id)=>{
-    //   return data.filter(item=>item.id!==id)
-    // }
- 
- 
+
+
   return (
     <div className={style.Foods}>
       <h1>{t("foods")}</h1>
       <div className={style.Select_block}>
         <div className={style.card}>
-          {data.map(a => <div  key={a.id} className={ style.Block_Card} onClick={()=>{click(a.id)}
-          }>
-          {language === "uz" ? (
-            <>{a.name}</>
-          ) : language === "en" ? (
-            <>{a.en_name}</>
-          ) : language === "ru" ? (
-            <>{a.ru_name}</>
-          ) : (
-            <>Mevalar bor edi!</>
-          )}
-          </div>)}
+          {data.map(a =><FoodItem deleteCard={()=>deleteCard(a)}  key={a.id} {...a}  id={a.id} />)}
         </div>
       </div>
       <div style={{ textAlign: "center" }}>
