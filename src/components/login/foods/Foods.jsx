c3e4e245a817b9b3777e22a3fc72009252072f18
Robiya <77631787+Robiyaxon@users.noreply.github.com>
2022-06-28 08:03:32 +0500

    pull

--- src/components/login/foods/Foods.jsx
+++ src/components/login/foods/Foods.jsx
@@ -6,41 +6,51 @@ import { instance } from '../../../api/api';
 import { useTranslation } from 'react-i18next';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import FoodItem from './FoodItem';
 const Foods = React.memo((props) => {
   const { t } = useTranslation();
   const [data, setData] = useState([]);
   const navigate = useNavigate();
   const [app, setApp] = useState([]);
   const { language } = useSelector((state) => state.langReducer);
-  const [selectedList, setSelectedList] = useState({});
   useEffect(() => {
     instance.get("product/").then((response) => setData(response.data));
   }, [setData]);
-  const click = async (id) => {
-
-    let temp = app;
-    temp.push(id)
-    setApp(temp);
-    setSelectedList(id)
-    return data.filter(item => item.id !== id)
-  }
   const click2 = () => {
     navigate("/femaleOrMale")
     props.setcan_not_dieta(app)
-    // return data.filter(item=>item.id!==id)
+
   };
-
-  const deleteCard =(id)=>setData(data => data.filter(item=>item.id!==id))
-  
-
+  console.log(app);
+  const deleteItem = (index, id) => () => {
+     setData((items) => items.filter((_, i) => i !== index))
+      let temp = app;
+      temp.push(id)
+      setApp(temp);
+      return data.filter(item => item.id !== id)
+    }
 
   return (
     <div className={style.Foods}>
       <h1>{t("foods")}</h1>
       <div className={style.Select_block}>
         <div className={style.card}>
-          {data.map(a =><FoodItem deleteCard={()=>deleteCard(a)}  key={a.id} {...a}  id={a.id} />)}
+          {data.map((it,index) => {
+            return (
+              <div key={it.id} >
+                <div key={it.id} className={style.Block_Card} onClick={deleteItem(index, it.id)}>
+                  {language === "uz" ? (
+                    <>{it.name}</>
+                  ) : language === "en" ? (
+                    <>{it.en_name}</>
+                  ) : language === "ru" ? (
+                    <>{it.ru_name}</>
+                  ) : (
+                    <>Mevalar bor edi!</>
+                  )}
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
       <div style={{ textAlign: "center" }}>
