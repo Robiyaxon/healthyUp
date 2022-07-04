5f0c2264590f1ba8e2ca0fa3667fe6c6ee11940e
TukhtayevaRoziya <muhammadumarmuhammadzoda@gmail.com>
2022-07-04 22:06:25 +0500

    some fix

--- src/components/login/loader/Loader.jsx
+++ src/components/login/loader/Loader.jsx
@@ -7,22 +7,22 @@ const Loader = () => {
   const navigate = useNavigate();
 
   setTimeout(() => {
-    var dom = document.getElementsByTagName("span") && document.getElementsByTagName("span")[4] ? document.getElementsByTagName("span")[4].textContent : "some error"
-    if (Number(dom) === 100) {
-      navigate('/conclusion')
-    } else {
-      console.log(dom === 100 + " error");
-    }
+    // var dom = document.getElementsByTagName("span") && document.getElementsByTagName("span")[4] ? document.getElementsByTagName("span")[4].textContent : "some error"
+    // if (Number(dom) === 100) {
+    //   navigate('/conclusion')
+    // } else {
+    //   console.log(dom === 100 + " error");
+    // }
   }, 10000);
 
   return (
-    <>
+    <div className={style.loader__body}>
       <h1 className={style.loader__title}>Ma'lumotlarni qayta ishlash</h1>
       <p  className={style.loader__text}>Sizning ovqatlanish rejangiz hisoblanmoqda...</p>
       <div className={style.loader}>
         <CountUp id={"count"} start={1} end={100} duration={9}></CountUp>
       </div>
-    </>
+    </div>
   );
 };
 
